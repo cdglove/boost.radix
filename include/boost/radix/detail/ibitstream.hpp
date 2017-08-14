cdc75e9f9fbc4eee9d8a790e470eb0b5aec68cb5
Chris Glover <cglover@defiant.nyc>
2017-08-14 17:59:44 -0400

    Convert streams to use reader object.

--- include/boost/radix/detail/ibitstream.hpp
+++ include/boost/radix/detail/ibitstream.hpp
@@ -37,13 +37,12 @@ public:
     {}
 
     template<typename ValueType, typename BitReader>
-    void read_bits(ValueType& v, BitReader& reader)
+    void read_bits(ValueType& v, BitReader const& reader)
     {
         BOOST_ASSERT(num_bits <= sizeof(v) * 8);
         BOOST_ASSERT(num_bits > 0);
 
         typedef typename std::iterator_traits<InputIterator>::value_type buffer_element_type;
-        typedef typename boost::make_unsigned<buffer_element_type>::type unsigned_buffer_element_type;
 
         // For now, we only support bytes.
         BOOST_STATIC_ASSERT(sizeof(buffer_element_type) == 1);
@@ -51,19 +50,19 @@ public:
         std::size_t const element_bits = sizeof(buffer_element_type) * 8;
 
         // Extract whatever is left in the current element.
-        v = unsigned_buffer_element_type(*current_) >> bit_; 
+        v = reader(*current_, bit_);
         
         std::size_t bits_extracted = element_bits - bit_;
     
-        if(bits_extracted == num_bits)
+        if(bits_extracted == reader.read_size_bits())
         {
             ++current_;
             bit_ = 0;
             return;
         }
-        else if(bits_extracted < num_bits)
+        else if(bits_extracted < reader.read_size_bits())
         {
-            std::size_t bits_remaining = num_bits - bits_extracted;
+            std::size_t bits_remaining = reader.read_size_bits() - bits_extracted;
             std::size_t aligned_bits_remaining = boost::alignment::align_up(bits_remaining, element_bits);
             std::size_t elements_remaining = aligned_bits_remaining / element_bits;
             BOOST_ASSERT(elements_remaining <= sizeof(v));
@@ -73,7 +72,7 @@ public:
             do
             {
                 ++current_;
-                ValueType rest = unsigned_buffer_element_type(*current_);
+                ValueType rest = reader(*current_, 0);
                 v |= rest << bits_extracted;
                 bits_extracted += element_bits;
             } while(--elements_remaining);
@@ -83,10 +82,10 @@ public:
         all_value_bit_mask = ~all_value_bit_mask;
 
         // Mask off any overhang we didn't ask for.
-        v &= ~(all_value_bit_mask << num_bits);
+        v &= ~(all_value_bit_mask << reader.read_size_bits());
 
         // Reset the bit offset to account for overhang.
-        bit_ += num_bits;
+        bit_ += reader.read_size_bits();
         while(bit_ >= 8)
         {
             bit_ -= 8;
@@ -127,12 +126,12 @@ public:
     template<typename SourceType>
     typename boost::make_unsigned<
         SourceType
-    >::type operator()(SourceType const& source, std::size_t source_bit_offset)
+    >::type operator()(SourceType const& source, std::size_t source_bit_offset) const
     {
         
     }
 
-    std::size_t read_size_bits()
+    std::size_t read_size_bits() const
     {
         return num_bits_;
     }
@@ -153,12 +152,12 @@ public:
     template<typename SourceType>
     typename boost::make_unsigned<
         SourceType
-    >::type operator()(SourceType const& source, std::size_t source_bit_offset)
+    >::type operator()(SourceType const& source, std::size_t source_bit_offset) const
     {
 
     }
 
-    std::size_t read_size_bits()
+    std::size_t read_size_bits() const
     {
         return num_bits_;
     }
